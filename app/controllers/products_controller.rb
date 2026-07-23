13a921a9e2ef172e86d024f56f5fe695651c77f8
mna84000 <mna84@academic.rrc.ca>
2026-07-22 21:34:56 -0500

    Add product pagination search and reusable product partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,27 @@
 class ProductsController < ApplicationController
   def index
-    @categories = Category.order(:name)
-    @products = Product.includes(:category)
-                       .where(active: true)
-                       .order(created_at: :desc)
+  @categories = Category.order(:name)
+
+  @products = Product.includes(:category)
+                     .where(active: true)
+
+  if params[:query].present?
+    keyword = "%#{ActiveRecord::Base.sanitize_sql_like(params[:query])}%"
+
+    @products = @products.where(
+      "products.name ILIKE :keyword OR products.description ILIKE :keyword",
+      keyword: keyword
+    )
   end
 
+  if params[:category_id].present?
+    @products = @products.where(category_id: params[:category_id])
+  end
+
+  @products = @products.order(created_at: :desc)
+                       .page(params[:page])
+                       .per(12)
+end
   def show
     @product = Product.includes(:category).find(params[:id])
   end
