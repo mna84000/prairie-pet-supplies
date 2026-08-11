83a2f5b839f57f1da4dd0a8794db6eafe1556aa2
mna84000 <mna84@academic.rrc.ca>
2026-08-11 18:26:55 -0500

    Add tag management to ActiveAdmin products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,32 +5,37 @@ ActiveAdmin.register Product do
                 :stock_quantity,
                 :active,
                 :category_id,
-                :image
+                :image,
+                tag_ids: []
 
   index do
-  selectable_column
-  id_column
+    selectable_column
+    id_column
 
-  column :image do |product|
-    if product.image.attached?
-      image_tag url_for(product.image), width: 60, height: 60
-    else
-      "No image"
+    column :image do |product|
+      if product.image.attached?
+        image_tag url_for(product.image), width: 60, height: 60
+      else
+        "No image"
+      end
     end
-  end
 
-  column :name
-  column :category
-  column :price
-  column :stock_quantity
-  column :active
-  actions
-end
+    column :name
+    column :category
+    column :price
+    column :stock_quantity
+    column :active
+    column :tags do |product|
+      product.tags.map(&:name).join(", ")
+    end
+    actions
+  end
 
   filter :name
   filter :category
   filter :price
   filter :active
+
 
   form do |f|
     f.inputs "Product Details" do
@@ -40,6 +45,7 @@ end
       f.input :stock_quantity
       f.input :active
       f.input :category
+      f.input :tags, as: :check_boxes
       f.input :image, as: :file
     end
 
@@ -47,25 +53,29 @@ end
   end
 
   show do
-  attributes_table do
-    row :id
-    row :name
-    row :description
-    row :price
-    row :stock_quantity
-    row :active
-    row :category
+    attributes_table do
+      row :id
+      row :name
+      row :description
+      row :price
+      row :stock_quantity
+      row :active
+      row :category
 
-    row :image do |product|
-      if product.image.attached?
-        image_tag url_for(product.image), width: 250
-      else
-        "No image uploaded"
+      row :tags do |product|
+        product.tags.map(&:name).join(", ")
       end
-    end
 
-    row :created_at
-    row :updated_at
+      row :image do |product|
+        if product.image.attached?
+          image_tag url_for(product.image), width: 250
+        else
+          "No image uploaded"
+        end
+      end
+
+      row :created_at
+      row :updated_at
+    end
   end
-end
 end
