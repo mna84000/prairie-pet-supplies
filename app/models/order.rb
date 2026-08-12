8f67aece9bcc94669a30d4de2719c873f57a75f4
mna84000 <mna84@academic.rrc.ca>
2026-08-12 12:26:36 -0500

    Add order status validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,6 +11,8 @@ class Order < ApplicationRecord
             :total,
             numericality: { greater_than_or_equal_to: 0 }
 
+ validates :status, presence: true
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[
       created_at
